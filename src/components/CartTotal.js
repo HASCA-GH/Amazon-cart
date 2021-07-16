1425df3823f26fcbd0135f7d69a39ee5bb63b6b6
Humberto Asca <hasca@ciplima.org.pe>
2021-07-16 12:47:20 -0500

    ADD:Cart total HTML/style

--- src/components/CartTotal.js
+++ src/components/CartTotal.js
@@ -3,7 +3,12 @@ import React from 'react'
 
 function CartTotal() {
     return (
-        <div className="CartTotal">Hello Cart Total</div>
+        <div className="CartTotal">
+            <h3>Subtotal (5 items): 
+                <span>$2172.38</span>
+            </h3>
+            <button>Proceed to checkout</button>
+        </div>
     )
 }
 
