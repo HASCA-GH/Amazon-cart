315fa1c75337bb243ed9f0c416925351cf73c946
Humberto Asca <hasca@ciplima.org.pe>
2021-07-16 14:06:48 -0500

    CHNG:Format total price

--- src/components/CartTotal.js
+++ src/components/CartTotal.js
@@ -1,5 +1,6 @@
 import '../css/CartTotal.css';
 import React from 'react'
+import NumberFormat from 'react-number-format';
 
 function CartTotal({items}) {
     const getTotalPrice = () => {
@@ -7,21 +8,50 @@ function CartTotal({items}) {
         // Loop through all the items in the cart
         // Add the price of item with the quantity to the total price
         // Return the total price
-
+    
         let total=0;
+        let totalitems=0;
         items.forEach((item) => {
             total += (item.price * item.quantity)
+            totalitems += item.quantity;
         })
-        return total;
+        // total = total.toFixed(2); //redondea a 2 decimales ... rounds to 2 decimals
+        return {total, totalitems};
     }
+
+    // without using react format number
+    // return (
+    //     <div className="CartTotal">
+    //         <h3>Subtotal ({getTotalPrice().totalitems} items): 
+    //             <span>{getTotalPrice().total}</span>
+    //         </h3>
+    //         <button>Proceed to checkout</button>
+    //     </div>
+    // )
+    
+    
+    // Using the react format number module:
+    // <NumberFormat value={2456981} displayType={'text'} thousandSeparator={true} prefix={'$'} />
+
+    // <NumberFormat value={getTotalPrice().total} displayType={'text'} thousandSeparator={true} prefix={'$'} />
     return (
         <div className="CartTotal">
-            <h3>Subtotal ({items.length} items): 
-                <span>{getTotalPrice()}</span>
+            <h3>Subtotal ({getTotalPrice().totalitems} items): 
+                <span>
+                    <NumberFormat 
+                        value={getTotalPrice().total} 
+                        displayType={'text'} 
+                        thousandSeparator={true} 
+                        prefix={'S/.'} 
+                        decimalScale={2}
+                    />
+                </span>
             </h3>
             <button>Proceed to checkout</button>
         </div>
     )
 }
+
+
 
 export default CartTotal
